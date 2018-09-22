596827bed16b1d57f7b6ff711e6db59269679873
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2018-09-22 10:19:05 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pepper Jack Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple CheeseBurger', true);
