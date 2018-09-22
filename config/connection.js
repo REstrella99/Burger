56f68d70cfed4aa0a701fc01056d520118eddc9a
REstrella99 <39837694+REstrella99@users.noreply.github.com>
2018-09-22 10:39:33 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,10 @@ if (process.env.JAWSDB_URL) {
 } else {
 
 	connection = mysql.createConnection({
-		port: 3306,
 		host: 'localhost',
 		user: 'root',
-		password: '',
-		database: 'burgers_db'
+		password: 'hacktheplanet',
+		database: 'todoagain_db'
 	})
 };
 
